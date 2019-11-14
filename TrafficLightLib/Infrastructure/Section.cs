b99ce881c39255ed98c30c2267dab647e3dce09b
MACE <d.mace@live.com>
2019-11-14 10:25:34 +0300

    + add <summary> of Section

--- TrafficLightLib/Infrastructure/Section.cs
+++ TrafficLightLib/Infrastructure/Section.cs
@@ -4,6 +4,13 @@ namespace TrafficLightLib.Infrastructure
 {
     public class Section
     {
+        /// <summary>
+        /// Sets status section of traffic light
+        /// </summary>
+        /// <param name="red">Sets status Red section</param>
+        /// <param name="yellow">Sets status Yellow section</param>
+        /// <param name="green">Sets status Green section</param>
+        /// <param name="lightTime">Sets time section</param>
         public Section(Status red, Status yellow, Status green, int lightTime = 1000)
         {
             Red = red;
@@ -12,22 +19,22 @@ namespace TrafficLightLib.Infrastructure
             LightTime = TimeSpan.FromMilliseconds(lightTime);
         }
         /// <summary>
-        /// Get State Red section of traffic light
+        /// Get status Red section of traffic light
         /// </summary>
         public Status Red { get; }
 
         /// <summary>
-        /// Get State Yellow section of traffic light
+        /// Get status Yellow section of traffic light
         /// </summary>
         public Status Yellow { get; }
 
         /// <summary>
-        /// Get State Green section of traffic light
+        /// Get status Green section of traffic light
         /// </summary>
         public Status Green { get; }
 
         /// <summary>
-        /// Get Time State
+        /// Get Time section
         /// </summary>
         public TimeSpan LightTime { get; }        
     }
